c17388de5b413f04e5eff28f89fdc381e908c627
chen-xwj <3383194298@qq.com>
2019-03-05 11:56:21 +0800

    修改了代码

--- src/main/java/com/neusoft/User.java
+++ src/main/java/com/neusoft/User.java
@@ -1,9 +1,11 @@
 package com.neusoft;
 
-public class Uesr {
+public class User {
 
     public static void main(String[] args) {
         System.out.println("提交 git 和 github");
-        System.out.println("李四改动的");
+
+       System.out.println("张三改动的");
+
     }
 }
